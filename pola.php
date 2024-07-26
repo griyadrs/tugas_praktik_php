f76064db4f83278c3fff88416e8a3ef6e9447d7a
griyadrs <maderestu775@gmail.com>
2024-07-26 12:29:07 +0700

    Add : Revision pola.php

--- pola.php
+++ pola.php
@@ -1,65 +1,82 @@
 <?php
+
 // Declatariton Variable
 $wide = 5;
 $distance = 0;
 
-// segitiga menurun
+// Descending Triangle
 for ($col = 1; $col <= $wide; $col++) {
-  for ($line = 1; $line <= $col; $line++) {
-      echo "* ";
-  }
-  echo "<br>";
+
+    for ($line = 1; $line <= $col; $line++) {
+        echo "* ";
+    }
+
+    echo "<br>";
 }
-echo "<br>";
+
+echo "<br>"; // Distance
+
 // Descending Triangle Refleksi
 for ($col = $wide; $col >= 1; $col--) {
-  for ($line = 1; $line <= $col; $line++) {
-      echo "* ";
-  }
-  echo "<br>";
+    
+    for ($line = 1; $line <= $col; $line++) {
+        echo "* ";
+    }
+
+    echo "<br>";
 }
-echo "<br>";
+
+echo "<br>"; // Distance
+
 // Descending Triangle v2
 for ($col = 1; $col <= $wide; $col++) {
-  $distance++;
-  for ($line = 1; $line <= $col; $line++) {
-      if ($distance == 3 and $line == 2) {
-          echo "# ";
-      } elseif ($distance == 4 and $line == 2) {
-          echo "# ";
-      } elseif ($distance == 4 and $line == 3) {
-          echo "# ";
-      } else {
-          echo "* ";
-      }
-  }
-  echo "<br>";
+    $distance++;
+
+    for ($line = 1; $line <= $col; $line++) {
+
+        if ($distance == 3 and $line == 2) {
+            echo "# ";
+        } elseif ($distance == 4 and $line == 2 || $line == 3) {
+            echo "# ";
+        } else {
+            echo "* ";
+        }
+    }
+
+    echo "<br>";
 }
-echo "<br>";
+
+echo "<br>"; // Distance
+
 // Rectangle
 $distance = 0;
+
 for ($col = 1; $col <= 3; $col++) {
-  $distance++;
-  for ($line = 1; $line <= $wide; $line++) {
-      if ($distance == 2 and $line == 3) {
-          echo "# ";
-      } else {
-          echo "* ";
-      }
-  }
-  echo "<br>";
+    $distance++;
+
+    for ($line = 1; $line <= $wide; $line++) {
+        if ($distance == 2 and $line == 3) {
+            echo "# ";
+        } else {
+            echo "* ";
+        }
+    }
+
+    echo "<br>";
 }
-echo "<br>";
+echo "<br>"; // Distance
+
 // Rectangle v2
 for ($col = 1; $col <= 3; $col++) {
-  $distance++;
-  for ($line = 1; $line <= $wide; $line++) {
-      if ($distance == 2 and $line == 3) {
-          echo "# ";
-      }
-      else {
-          echo "* ";
-      }
-  }
-  echo "<br>";
+    $distance++;
+    
+    for ($line = 1; $line <= $wide; $line++) {
+        if ($distance == 2 and $line == 3) {
+            echo "# ";
+        } else {
+            echo "* ";
+        }
+    }
+
+    echo "<br>";
 }
